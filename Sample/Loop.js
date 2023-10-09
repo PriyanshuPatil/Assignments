78a71a726fd4e1bd4a43b16a1f590cf8fe689a30
PriyanshuPatil <pantuugurjar@gmail.com>
2023-10-09 20:10:56 +0530

    edited

--- Sample/Loop.js
+++ Sample/Loop.js
@@ -2,3 +2,11 @@ let str="Priyanshu" ;
 for(let i=0;i<5;i++){
   console.log(str[i]) ;
 }
+
+for(let i=0;i<5;i++){
+  let name=""
+  for(let j=0;j<5;j++){
+    name=name+str[i];
+    console.log(name)
+  }
+}
